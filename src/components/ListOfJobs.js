aaf348ee7ab0580c1d13d69eea8489f7994bf7fd
Jakasco <kasperi.mutku@elisanet.fi>
2021-01-07 20:03:15 +0200

    json fetching working

--- src/components/ListOfJobs.js
+++ src/components/ListOfJobs.js
@@ -1,15 +1,18 @@
 import React, { useState } from 'react';
 import {Link} from 'react-router-dom';
 
-const ListOfJobs = () => {
+const ListOfJobs = (props) => {
 
 const [dynamicElems, setDynamicElems] = useState([]);
 
+
  const myRef = React.createRef();
 
-  const addJob = () => {
+console.log("props ListOfJobs: ",props.jobs);
 
-    const newDynamicModal = <div ref={myRef}>
+const addJob = () => {
+
+const newDynamicModal = <div ref={myRef}>
          <li>
          <Link to="/jobview"><button>
          Job
@@ -19,10 +22,63 @@ const [dynamicElems, setDynamicElems] = useState([]);
     </div>;
 
     setDynamicElems(() => [...dynamicElems, newDynamicModal]);
-  };
+};
+
+const addJobFromJson = (jsonObj) => {
+
+    const newDynamicModal = <div ref={myRef}>
+             <li>
+             <Link to="/jobview"><button>
+             {jsonObj.Name}
+                </button>
+                </Link>
+                ID: {jsonObj.ID}
+                 </li>
+        </div>;
+    
+        setDynamicElems(() => [...dynamicElems, newDynamicModal]);
+    };
+
+let jobsFetched = false;
+
+const getJobsJson = () => {
+    console.log("Get jobs done?");
+  /*  jobsFetched = true;  //Stop the loop
+    fetch('../jobs.json'
+    ,{
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+    }
+    )
+      .then(function(response){
+        console.log(response)
+        return response.json();
+      })
+      .then(function(myJson) {
+    //    console.log(myJson);
+        let myJson = props.jobs;
+        console.log("myJson? : ",myJson);
+        if(jobsFetched == false){
+        for (var key in myJson) {
+            if (myJson.hasOwnProperty(key)) {
+              console.log(myJson[key].ID);
+              console.log(myJson[key].Name);
+              addJobFromJson(myJson);
+         }
+        }
+        jobsFetched = true;
+    }*/
+    //  });
+};
+
+getJobsJson();
+
 
   return (
     <div >
+        <h2>name. {props.name}</h2>
        <div>Jobs:
             <ul>
             {dynamicElems}
--- src/views/Home.js
+++ src/views/Home.js
@@ -12,11 +12,79 @@ class Home extends Component {
 
   state = {
     jobName: 'asd',
+    jobs: "asdddd",
 };
 
-componentDidMount() {
-    console.log("props home: ",this.props);
+
+
+    
+
+
+addItem = (myJson) => {
+    this.setState({
+        jobs: [
+        ...this.state.jobs,
+        myJson 
+      ]
+    })
+  };
+
+getJson = () => {fetch('../jobs.json'
+,{
+  headers : { 
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+   }
 }
+)
+  .then(function(response){
+    console.log(response)
+    return response.json();
+  })
+  .then(function(myJson) {
+    console.log(myJson);
+   // this.setState({jobName: "asdsadada"});
+
+  //  this.setState({jobs: "asd"});
+  //  this.addItem(myJson);
+
+  return myJson;
+
+  /*
+  this.setState({
+    jobs: [
+    ...this.state.jobs,
+    myJson 
+  ]
+});*/
+  });
+}
+
+
+componentDidMount() {
+
+    const that = this;
+    console.log("props home: ",this.props);
+
+    fetch('../jobs.json'
+    ,{
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+    })
+      .then(function(response){
+      //  console.log(response)
+        return response.json();
+      })
+      .then(function(myJson) {
+        console.log(myJson);
+
+      that.setState({ jobs: myJson });
+      });
+};
+
+
 
 render()
 {
@@ -25,11 +93,10 @@ render()
 
       <h1>Home: {this.state.jobName}</h1>
         <div>
-             <h1>Jobs</h1>
-             <h1>Users</h1>
-             <h1>Customers</h1>
+             <h1>Jobs:</h1>
+           
         </div>
-        <ListOfJobs />
+        <ListOfJobs jobs={this.state.jobs} />
       </div>
   );
 };
