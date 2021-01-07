9de51c7163cfba4b342f78f92dece9206abde575
Jakasco <kasperi.mutku@elisanet.fi>
2021-01-07 15:09:33 +0200

    adding modal at mouse position function added

--- src/components/AddModal.js
+++ src/components/AddModal.js
@@ -10,7 +10,15 @@ const AddModal = () => {
     float: "left",
     width: "70%",
     backgroundColor: "rgb(95 95 95)",
+    
   };
+/*
+  const modalStyle = {
+      height: "10vh",
+      width: "40%",
+      border: "solid black 1px",
+      borderRadius: "10px",
+  }*/
 
   const addElement = (event) => {
     console.log("asd ",event);
@@ -20,17 +28,40 @@ const AddModal = () => {
         y: event.screenY,
       };
 
+      const modalStyle2 = {
+        zIndex: "10",
+        height: "10vh",
+        width: "30%",
+        border: "solid black 1px",
+        borderRadius: "10px", 
+        position: "absolute",
+        left: event.pageX + 'px',
+        top: event.pageY + 'px'
+    }
+
+
+
+
       console.log("coords: ",coords);
-      
+
     // Creates the dynamic paragraph
-    const newDynamicElem = <p className={"asd"}>This is paragraph</p>;
+ //   const newDynamicElem = <p className={"asd"}>This is paragraph</p>;
+
+    const newDynamicModal = <div style={modalStyle2}>
+        <button>Create task</button>
+        <button>Assign task</button>
+    </div>;
+
     // adds it to the state
-    setDynamicElems(() => [...dynamicElems, newDynamicElem]);
+    setDynamicElems(() => [...dynamicElems, newDynamicModal]);
   };
 
   return (
     <div>
-      <div onClick={(event) => addElement(event)}  style={taskWindowStyle}>Click here{dynamicElems}</div>
+      <div onClick={(event) => addElement(event)}  
+             style={taskWindowStyle}>
+          Click here{dynamicElems}
+       </div>
     </div>
   );
 };
--- src/components/TasksWindow.js
+++ src/components/TasksWindow.js
@@ -8,57 +8,12 @@ import { useState } from 'react';
 
 class TasksWindow extends Component {
 
-myRef = React.createRef();
-
 handleTaskSubmit = (evt) => {
 
 };
 
-
-
-
-
-componentDidMount() {
-/*
-  const node = this.myRef.current;
-
- // const [dynamicElems, setDynamicElems] = useState([]);
-
-  node.addEventListener('click', (event) => {
-    console.log("asd ",event);
-    let coords = {
-      x: event.screenX,
-      y: event.screenY,
-    };
-
-    console.log("coords: ",coords);
- 
-  });*/
-}
-
-
-  
- // const [dynamicElems, setDynamicElems] = useState([]);
-/*
-addElement = () => {
-  console.log("Add element");
-    // Creates the dynamic paragraph
-    const newDynamicElem = <p className={classes.elements}>This is paragraph</p>;
-    // adds it to the state
-    setDynamicElems(() => [...dynamicElems, newDynamicElem]);
-};*/
-  
-// style={taskWindowStyle} className="taskWindow" ref={this.myRef}
-
 render()
 {
-  const taskWindowStyle = {
-    height: "70vh",
-    float: "left",
-    width: "70%",
-    backgroundColor: "rgb(95 95 95)",
-  };
-
     return (
 
         <div> 
